25185458bdc3dc612ed3a50adb5845e5b0f57495
kjones030371 <christian.krienzer@gmx.net>
2022-02-04 10:16:16 +0100

    initial Version

--- Kassensystem.py
+++ Kassensystem.py
@@ -1,24 +1,32 @@
 import locale
 
-
 locale.setlocale(locale.LC_ALL, 'de_DE')
 weitere_berechnung = 'J'
+umsatz = 0
 
-print("Kassensystem")
-
-while weitere_berechnung == 'J' or weitere_berechnung == 'j':
+def kassenvorgang():
     preis = input('Gib den Preis ein:\t\t')
     rabatt = input('Gib den Rabatt in % ein:\t')
     zahlung = input('Der Kunde zahlt: \t\t ')
     preis = locale.atof(preis)
     zahlung = locale.atof(zahlung)
     rabatt_in_prozent = locale.atof(rabatt)
-    rabatt_in_euro = (preis/100) * rabatt_in_prozent
+    rabatt_in_euro = (preis / 100) * rabatt_in_prozent
     rabatt_preis = preis - rabatt_in_euro
-    rueckgeld = zahlung - rabatt_preis
-    print('Der Rabatt beträgt: ', locale.format_string('%.2f', rabatt_in_euro))
-    print('Der neu berechnete Preis ist: ', locale.format_string('%.2f', rabatt_preis))
-    print('Wechselgeld:\t\t', locale.format_string('%.2f', rueckgeld,))
+    if rabatt_preis > zahlung:
+        print("Die Zahlung ist zu wenig -- Bitte um Neueingabe ")
+        kassenvorgang()
+    else:
+        rueckgeld = zahlung - rabatt_preis
+        print('Der Rabatt beträgt: ', locale.format_string('%.2f', rabatt_in_euro))
+        print('Der neu berechnete Preis ist: ', locale.format_string('%.2f', rabatt_preis))
+        print('Wechselgeld:\t\t', locale.format_string('%.2f', rueckgeld, ))
+    return rabatt_preis
+
+print("Kassensystem")
+while weitere_berechnung == 'J' or weitere_berechnung == 'j':
+    umsatz = umsatz + kassenvorgang()
     weitere_berechnung = input("Haben Sie noch eine weitere Eingabe (J == Ja oder N == nein): ")
+print("Der Umsatz beträgt: ", umsatz)
 print("Programmende")
 
--- Schroedinger.py
+++ Schroedinger.py
@@ -1,18 +1,12 @@
-_wert = "Hallo"
-wort1 = "Schrödinger"
-wörtlich = "wie"
-wort_drei = "geht's"
 
-print(_wert, wort1, wörtlich, wort_drei, sep='-', end='?\n')
+def zahlendreher(zahl):
+    if zahl > 5:
+        zahl = zahl - 1
+        print(zahl)
+        zahl = zahlendreher(zahl)
+        return zahl
+    return zahl
 
-spam = "10" * 3
-print(type(spam))
-
-egg = int("42") > 30
-print(type(egg))
-
-nose = 12.0 * 3
-print(type(nose))
-
-ergebnis = egg * nose
-print(type(ergebnis))
+zahl = int(input("Gib eine Zahl ein: "))
+zahl = zahlendreher(zahl)
+print(zahl)
