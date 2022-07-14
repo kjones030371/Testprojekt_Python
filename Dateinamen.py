de138ebdb62b2a999db55a18a2f74c2302f6f037
kjones030371 <christian.krienzer@gmx.net>
2022-07-14 13:25:35 +0200

    date: 140721 - Seite 167

--- Dateinamen.py
+++ Dateinamen.py
@@ -1,15 +1,12 @@
-datei_name = "Grabung-42-Ebene1-jpg"
-print(len(datei_name))
+def kurzer_dateiname(datei_name):
+    anfang = datei_name[0:6]
+    ende = datei_name[-3:]
+    trenner = datei_name.index('-')
+    zahlen = datei_name[trenner + 1:trenner + 3]
+    neuer_name = anfang.lower() + zahlen + '.' + ende
+    return neuer_name
 
-anfang = datei_name[0:6]
-print(anfang)
+namen=('Grabung-42-Ebene1-jpg','Grabung-92-Hoehl92.pdf','Dschungel-42-Gebaeude.jpg')
+for name in namen:
+ print (kurzer_dateiname(name))
 
-ende = datei_name[-3:]
-print(ende)
-
-trenner = datei_name.index('-')
-print(trenner)
-
-zahlen = datei_name[trenner+1:trenner+3]
-print(zahlen)
-# So soll es sein
