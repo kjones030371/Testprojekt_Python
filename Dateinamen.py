21616f66feb8fb4436950ed2f66770f457e7405c
kjones030371 <christian.krienzer@gmx.net>
2022-07-14 13:45:05 +0200

    date: 140721 - Seite 192

--- Dateinamen.py
+++ Dateinamen.py
@@ -3,7 +3,8 @@ def kurzer_dateiname(datei_name):
     ende = datei_name[-3:]
     trenner = datei_name.index('-')
     zahlen = datei_name[trenner + 1:trenner + 3]
-    neuer_name = anfang.lower() + zahlen + '.' + ende
+    anfang = anfang.lower()
+    neuer_name = f"{anfang}{zahlen}.{ende}"
     return neuer_name
 
 namen=('Grabung-42-Ebene1-jpg','Grabung-92-Hoehl92.pdf','Dschungel-42-Gebaeude.jpg')
