91e2d944a6aa36b826ede4d61633b96911def144
kjones030371 <christian.krienzer@gmx.net>
2022-07-15 11:35:05 +0200

    date: 150721 - Seite 200

--- Dateinamen.py
+++ Dateinamen.py
@@ -4,10 +4,18 @@ def kurzer_dateiname(datei_name):
     trenner = datei_name.index('-')
     zahlen = datei_name[trenner + 1:trenner + 3]
     anfang = anfang.lower()
-    neuer_name = f"{anfang}{zahlen}.{ende}"
-    return neuer_name
+    if anfang.isalpha() and ende.isalpha() and zahlen.isnumeric():
+        neuer_name = f"{anfang}{zahlen}.{ende}"
+        neuer_name = neuer_name.lower()
+        return neuer_name
+    else:
+        None
 
-namen=('Grabung-42-Ebene1-jpg','Grabung-92-Hoehl92.pdf','Dschungel-42-Gebaeude.jpg')
+namen=('Grabung-42-Ebene1-jpg','Grabung-xx-Hoehl92.pdf','Dschungel-42-Gebaeude.jpg')
 for name in namen:
- print (kurzer_dateiname(name))
+    neuer_name = kurzer_dateiname(name)
+    laenge = len(name)
+    ergebnis = f"'{name}' mit {laenge} Zeichen. Neuer Name: '{neuer_name}'"
+    print(ergebnis)
+
 
