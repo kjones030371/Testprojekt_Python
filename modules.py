85b5de6bb7c8e645792af8d2e1017ea4c068fff2
kjones030371 <christian.krienzer@gmx.net>
2023-03-23 11:23:47 +0100

    part 1

---
+++ modules.py
@@ -0,0 +1,4 @@
+import spam
+ergebnis = spam.mal10(2)
+print(spam.mal10(42))
+help(spam)
---
+++ spam.py
@@ -0,0 +1,6 @@
+"""Dies ist ein Modul zum Ausprobieren."""
+irgendein_interner_wert = 42
+def mal10(wert):
+    '''Diese Funktion nimmt einen Wert mal 10.'''
+    ergebnis = wert *10
+    return ergebnis
